a585bfef0b877fa8683adbc907272b2bcee5f4d5
Naim <skylike2904@gmail.com>
2022-07-31 00:30:32 +0600

    added crud routes

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,11 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+posts_db = list()
+
+class Post(BaseModel):
+    id: Optional[int] = None
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[int] = None
+
+# GET
 @app.get("/")
 async def root():
-    return {"message": "hello ohh my api"}
+    return {"message": "hello world!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"message": "message sent..."}
+    if not posts_db:
+        raise HTTPException(status_code=404, detail="no post available!")
+    return {"data": posts_db}
+
+@app.get("/posts/latest")
+def get_latest_post():
+    try: return {"data": posts_db[len(posts_db)-1]}
+    except:
+        raise HTTPException(status_code=404, detail="no post available!")
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    try: return {f"post no.{id}": posts_db[id]}
+    except: raise HTTPException(status_code=404, detail=f"{id} doesn't exist!")
+    
+# POSTS
+@app.post("/posts", status_code=201)
+def create_post(post: Post):
+    post.id = len(posts_db)
+    posts_db.append(post.dict())
+    print(posts_db)
+    return {"data": post}
+
+#DELETE
+@app.delete("/posts/{id}", status_code=204)
+def delete_post(id: int):
+    try:
+        posts_db.pop(id)
+        return Response(status_code=204)
+    except:
+        raise HTTPException(status_code=404, detail=f"post:{id} doesn't exist!")
+    
+#UPDATE
+@app.put("/posts/{id}", status_code=200)
+def update_post(id: int, post: Post):
+    post.id = id
+    try:
+        posts_db[id] = post.dict()
+        return {"updated post": post}
+    except:
+        raise HTTPException(status_code=404, detail=f"post:{id} doesn't exist!")
