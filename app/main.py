642c7df1751e782f79e43fbea8f38a7fd1e4ab77
Naim <skylike2904@gmail.com>
2022-08-01 21:51:31 +0600

    added orm and porsgres as db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, Response, HTTPException
+from fastapi import FastAPI, Response, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
+import psycopg2
+from  psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -16,85 +24,147 @@ app.add_middleware(
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
-    
 )
 
-posts_db = [
-    {
-        "id": 0,
-        "title": "post title",
-        "content": "post details...",
-        "published": False,
-        "rating": 3
-    },
-    {
-        "id": 1,
-        "title": "post title",
-        "content": "post details...",
-        "published": False,
-        "rating": 3
-    },
-    {
-        "id": 2,
-        "title": "post title",
-        "content": "post details...",
-        "published": False,
-        "rating": 3
-    }
-]
-
 class Post(BaseModel):
-    id: Optional[int] = None
     title: str
     content: str
     published: bool = False
-    rating: Optional[int] = None
+
+# CONNECTION TO THE DATABASE USING DEFAULT DRIVER
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='whatwentwrong', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("db connection successful...")
+#         break
+#     except Exception as error:
+#         print("db connection failed!")
+#         print(error)
+#         time.sleep(2)
 
 # GET
 @app.get("/")
-async def root():
-    return {"message": "hello world!"}
+def root():
+    return {"message": "add '/docs' after the url to get the documentations!"}
 
-@app.get("/posts")
-def get_posts():
-    if not posts_db:
+#USING DEFAULT DRIVER
+# @app.get("/posts")
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC;""")
+#     res = cursor.fetchall()
+    
+#     if not res:
+#         raise HTTPException(status_code=404, detail="no post available!")
+#     else: 
+#         return {"posts": res}
+
+# @app.get("/posts/latest")
+# def get_latest_post():
+#     cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC LIMIT 1;""")
+#     res = cursor.fetchone()
+    
+#     if not res:
+#         raise HTTPException(status_code=404, detail="no posts available!")
+#     else:
+#         return {"latest post": res}
+
+# @app.get("/posts/{id}")
+# def get_post(id: int):
+#     cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (str(id)))
+#     res = cursor.fetchone()
+    
+#     if not res:
+#         raise HTTPException(status_code=404, detail=f"post with id={id} doesn't exist!")
+#     else:
+#         return {f"post with id={id}": res}
+
+#USING ORM SQL ALCHEMY
+@app.get('/posts')
+def test_posts(db: Session = Depends(get_db)):
+    res = db.query(models.Post).order_by(models.Post.created_at).all()
+    
+    if not res:
         raise HTTPException(status_code=404, detail="no post available!")
-    return {"data": posts_db}
+    return {"posts": res}
 
 @app.get("/posts/latest")
-def get_latest_post():
-    try: return {"data": posts_db[len(posts_db)-1]}
-    except:
-        raise HTTPException(status_code=404, detail="no post available!")
+def get_latest_post(db: Session=Depends(get_db)):
+    res = db.query(models.Post).order_by(models.Post.created_at.desc()).first()
+    
+    if not res:
+        raise HTTPException(status_code=404, detail="no posts available!")
+    return {"latest post": res}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    try: return {f"post no.{id}": posts_db[id]}
-    except: raise HTTPException(status_code=404, detail=f"{id} doesn't exist!")
+def get_post(id: int, db: Session=Depends(get_db)):
+    res = db.query(models.Post).filter(models.Post.id == id).first()
     
+    if not res:
+        raise HTTPException(status_code=404, detail=f"post with id={id} doesn't exist!")
+    return {f"post with id={id}": res}
+
 # POSTS
+# USING DEFAULT DRIVER
+# @app.post("/posts", status_code=201)
+# def create_post(post: Post):
+#     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *;""", (post.title, post.content, post.published))
+#     res = cursor.fetchone()
+#     conn.commit()
+    
+#     return {"post created successfully": res}
+
+#USING ORM
 @app.post("/posts", status_code=201)
-def create_post(post: Post):
-    post.id = len(posts_db)
-    posts_db.append(post.dict())
-    print(posts_db)
-    return {"data": post}
+def create_post(post: Post, db: Session=Depends(get_db)):
+    res = models.Post(**post.dict())
+    db.add(res)
+    db.commit()
+    db.refresh(res)
+    
+    return {"post created successfully": res}
 
 #DELETE
+# @app.delete("/posts/{id}", status_code=204)
+# def delete_post(id: int):
+#     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *;""", (str(id)))
+#     res = cursor.fetchone()
+#     conn.commit()
+    
+#     if not res:
+#         raise HTTPException(status_code=404, detail=F"post with id={id} doesn't exist!")
+#     else:
+#         return Response(status_code=204)
+
+#USING ORM
 @app.delete("/posts/{id}", status_code=204)
-def delete_post(id: int):
-    try:
-        posts_db.pop(id)
-        return Response(status_code=204)
-    except:
-        raise HTTPException(status_code=404, detail=f"post:{id} doesn't exist!")
+def delete_post(id: int, db: Session=Depends(get_db)):
+    res = db.query(models.Post).filter(models.Post.id == id)
+    
+    if not res.first():
+        raise HTTPException(status_code=404, detail=F"post with id={id} doesn't exist!")
+    res.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=204)
     
 #UPDATE
 @app.put("/posts/{id}", status_code=200)
-def update_post(id: int, post: Post):
-    post.id = id
-    try:
-        posts_db[id] = post.dict()
-        return {"updated post": post}
-    except:
-        raise HTTPException(status_code=404, detail=f"post:{id} doesn't exist!")
+# def update_post(id: int, post: Post):
+#     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;""", (post.title, post.content, post.published, str(id)))
+#     res = cursor.fetchone()
+#     conn.commit()
+    
+#     if not res:
+#         raise HTTPException(status_code=404, detail=f"post with id={id} doesn't exist!")
+#     else:
+#         return {"updated post": res}
+
+#USING ORM
+def update_post(id: int, post: Post, db: Session=Depends(get_db)):
+    res = db.query(models.Post).filter(models.Post.id == id)
+    
+    if not res.first():
+        raise HTTPException(status_code=404, detail=f"post with id={id} doesn't exist!")
+    res.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"updated post": res.first()}
